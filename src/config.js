3eeb754830c8315767f0abcda9d785e81c06f0d0
Sriram R <sriramranganathan1971@gmail.com>
2025-09-09 12:06:04 +0530

    Normalize line endings

--- src/config.js
+++
@@ -1,3 +0,0 @@
-// src/config.js
-// If you use Vite, you can instead set VITE_API_URL in .env and use import.meta.env.VITE_API_URL
-export const API = (import.meta?.env?.VITE_API_URL) || "http://localhost:5000";
